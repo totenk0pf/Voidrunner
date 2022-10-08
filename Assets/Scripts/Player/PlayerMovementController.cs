5e406521d4bb2b10e9adf0b2563fb153a2714234
Griffin <51525281+duong-lc@users.noreply.github.com>
2022-10-08 13:33:31 +0700

    prototype player controller finish until further notice

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
 
 public class PlayerMovementController : MonoBehaviour
@@ -30,6 +32,15 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] private float accelForce;
     [SerializeField] private float maxSpeed;
 
+    [Header("Movement Drag")] 
+    public bool canDrag = true;
+    public float rigidBodyDrag = 3;
+    
+    [Header("Slope Handling")] 
+    [SerializeField] private float slopeCastDist;
+    public float maxSlopeAngle;
+    private RaycastHit _slopeHit;
+
     private Rigidbody _rb;
     private Vector3 _moveDir;
 
@@ -40,11 +51,14 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField]  private KeyCode rollKey;
     [SerializeField] private float rollDistance;
     [SerializeField] private float rollTime;
-    [SerializeField] private float routineInterval;
 
     [Header("Slide Attributes")] 
+    public bool toggleProtoSlide = true;
+    [Space]
     [SerializeField] private KeyCode slideKey;
     [SerializeField] private float slideTime;
+    [SerializeField] private float slideDistance;
+    
 
     private void Awake()
     {
@@ -55,27 +69,49 @@ public class PlayerMovementController : MonoBehaviour
     private float _vert;
     private void FixedUpdate()
     {
-        _horiz = Input.GetAxis("Horizontal");
-        _vert = Input.GetAxis("Vertical");
-        _moveDir = new Vector3(_horiz, 0 , _vert).normalized;
-        
-        
-        
-        if(moveState == MovementState.Normal) UpdateStrafe(_moveDir);
+        UpdateMoveDir();
+        if(moveState == MovementState.Normal) UpdateStrafe();
+        if (canDrag) GivingDragOnSlope();
     }
 
     private void Update()
     {
+        
         if(Input.GetKeyDown(rollKey)) ActionRoll(_moveDir);
+        if(Input.GetKeyDown(slideKey)) ActionSlide(_moveDir);
     }
 
 
-    private void UpdateStrafe(Vector3 moveDir)
+    private void UpdateStrafe()
     {
-        _rb.AddForce(UpdateDirChangeAccelTamper(moveDir) * accelForce * moveDir);
+        if (OnSlope()) {
+            _rb.AddForce(UpdateDirChangeAccelTamper(_moveDir) * accelForce * _moveDir);
+        }
+        else {
+            _rb.AddForce(UpdateDirChangeAccelTamper(_moveDir) * accelForce * _moveDir);
+        }
+        
+        ClampMaxSpeed();
+    }
 
-        Vector3 rbVel = _rb.velocity;
-        if (rbVel.magnitude > maxSpeed) _rb.velocity = Vector3.ClampMagnitude(rbVel, maxSpeed); //clamping maxSpeed
+    private void ClampMaxSpeed()
+    {
+        if (OnSlope())
+        {
+            if (_rb.velocity.magnitude > maxSpeed)
+                _rb.velocity = _rb.velocity.normalized * maxSpeed;
+        }
+        else
+        {
+            Vector3 rbVel = _rb.velocity;
+            var relVel = new Vector3(rbVel.x, 0, rbVel.z);
+
+            if (relVel.magnitude > maxSpeed)
+            {
+                relVel = Vector3.ClampMagnitude(rbVel, maxSpeed);
+                _rb.velocity = new Vector3(relVel.x, _rb.velocity.y, relVel.z);
+            }
+        }
     }
 
     //normalize rigid vel vector and input vector
@@ -96,7 +132,54 @@ public class PlayerMovementController : MonoBehaviour
         else
             return accelRate;
     }
-    
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out _slopeHit, slopeCastDist))
+        {
+            float angle = Vector3.Angle(Vector3.up, _slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+        return false;
+    }
+
+    private void GivingDragOnSlope()
+    {
+        if (!OnSlope()) {
+            _rb.drag = 0;
+            _rb.useGravity = true;
+            return;
+        }
+        
+        float value = Vector3.Dot(_rb.velocity.normalized, Vector3.up);
+        //going down
+        if (value < 0) { 
+            _rb.drag = rigidBodyDrag;
+            _rb.useGravity = false;
+        }
+        else {
+            _rb.drag = 0;
+            _rb.useGravity = true;
+        }
+
+        if (_rb.velocity.magnitude < 0.3f)
+            _rb.useGravity = true;
+
+    }
+
+    private Vector3 UpdateMoveDir()
+    {
+        _horiz = Input.GetAxis("Horizontal");
+        _vert = Input.GetAxis("Vertical");
+        _moveDir = transform.forward * _vert + transform.right * _horiz;
+        
+        if(OnSlope())
+        {
+            _moveDir = Vector3.ProjectOnPlane(_moveDir.normalized, _slopeHit.normal.normalized);
+        }
+
+        return _moveDir;
+    }
     
     /// <summary>
     /// Roll the character
@@ -107,7 +190,7 @@ public class PlayerMovementController : MonoBehaviour
     private void ActionRoll(Vector3 moveDir)
     {
         //check for movement and prevent roll stack
-        if (moveDir.magnitude == 0 || moveState == MovementState.Roll) { return; }
+        if (moveDir.magnitude == 0 || moveState != MovementState.Normal) { return; }
 
         moveState = MovementState.Roll;
         
@@ -115,10 +198,10 @@ public class PlayerMovementController : MonoBehaviour
         Vector3 dest = transform.position + (moveDir) * rollDistance;
         Vector3 rollAxis = Vector3.Cross(moveDir, playerVisualProto.up);
         
-        StartCoroutine(LerpPosRoutine(dest, rollAxis));
+        StartCoroutine(LerpRollRoutine(dest, rollAxis));
     }
     
-    private IEnumerator LerpPosRoutine(Vector3 dest, Vector3 rollAxis)
+    private IEnumerator LerpRollRoutine(Vector3 dest, Vector3 rollAxis)
     {
         var startPos = transform.position;
         float time = 0;
@@ -128,7 +211,7 @@ public class PlayerMovementController : MonoBehaviour
             transform.position = Vector3.Lerp(startPos, dest, time / rollTime);
             time += Time.deltaTime;
             
-            if(toggleProtoRoll)
+            if(toggleProtoRoll && moveState == MovementState.Roll)
                 playerVisualProto.RotateAround(transform.position, rollAxis, rollAngleProto);
             
             
@@ -140,15 +223,49 @@ public class PlayerMovementController : MonoBehaviour
         moveState = MovementState.Normal;
     }
     
-    //For Sliding
-    //check for velocity from rb
-    //if reached max speed -> let player slide (lerp)
+    
     private void ActionSlide(Vector3 moveDir)
     {
-        if (Mathf.Abs(_rb.velocity.magnitude - maxSpeed) <= 0.1f)
+        if (Mathf.Abs(_rb.velocity.magnitude - maxSpeed) > 0.5f || moveState != MovementState.Normal) { return; }
+
+        moveState = MovementState.Slide;
+        //var velMagCache = _rb.velocity.magnitude;
+        Vector3 dest = transform.position + moveDir * slideDistance;
+        Vector3 pointAxis = moveDir;
+
+        StartCoroutine(LerpSlideRoutine(dest, pointAxis));
+
+    }
+
+    
+    private IEnumerator LerpSlideRoutine(Vector3 dest, Vector3 pointAxis)
+    {
+        var startPos = transform.position;
+        float time = 0;
+
+        if (toggleProtoSlide && moveState == MovementState.Slide)
         {
-            //_rb.AddForce(  * moveDir);
+            playerVisualProto.up = -pointAxis;
         }
+        
+        while (time < slideTime)
+        {
+            transform.position = Vector3.Lerp(startPos, dest, time / slideTime);
+            time += Time.deltaTime;
+            
+            yield return null;
+        }
+        
+        transform.position = dest;
+        playerVisualProto.up = Vector3.up;
+        moveState = MovementState.Normal;
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, _moveDir * 3);
     }
 }
 
