6723e16afede43d12566eaf7ce3d8f49290855d9
legendofnothing <huyrtb123@gmail.com>
2022-10-18 10:35:02 +0700

    Revert "Add skillpoints and skilltypes and related methods"
    This reverts commit bf4cc5f78033685c4a0577374be5958c95a8115b.

--- Assets/Scripts/Player/PlayerProgession.cs
+++ Assets/Scripts/Player/PlayerProgession.cs
@@ -5,30 +5,20 @@ using UnityEngine;
 public class PlayerProgession : MonoBehaviour
 {
     public int level;
-    public int skillPoints;
     public float currentXP;
-    public float levelupXP = 50;
+    public float levelupXP;
 
-    [Space]
-    public int vigor = 1;
-    public int endurance = 1;
-    public int strength = 1;
-    public int dexterity = 1;
-
-    //God i love chisato
-
-    public enum SkillType
-    {
-        Vigor, 
-        Endurance,
-        Strength,
-        Dexterity
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.O)) {
+            Debug.Log("Current XP: " + currentXP + ". Level Up XP: " + levelupXP + ". Level: " + level);
+            AddXP(3);
+        }
     }
 
     public void AddXP(float amount) {
         if (currentXP + amount >= levelupXP) {
             currentXP = (currentXP + amount) - levelupXP;
-            levelupXP = 50 * Mathf.Pow(1.2f, level);
+            levelupXP += 10;
             LevelUp();
         }
 
@@ -39,44 +29,5 @@ public class PlayerProgession : MonoBehaviour
 
     public void LevelUp() {
         level++;
-        skillPoints++;
     }
-
-    public void AddSkillType(SkillType type) {
-        if (skillPoints > 0) {
-            switch (type) {
-                case SkillType.Vigor:
-                    vigor++;
-
-                    skillPoints--;
-                    break;
-
-                case SkillType.Endurance:
-                    endurance++;
-
-
-                    skillPoints--;
-                    break;
-
-                case SkillType.Strength:
-                    strength++;
-
-
-                    skillPoints--;
-                    break;
-
-                case SkillType.Dexterity:
-                    dexterity++;
-
-
-                    skillPoints--;
-                    break;
-
-                default:
-                    Debug.Log("Invalid Skill Type in " + this);
-                    break;
-            }
-        }
-    }
-    
 } 
