be8d5003b1e6727167e7b037e159d65466a328d7
totenk0pf <nguyenhuuquang2608@gmail.com>
2022-10-25 12:16:02 +0700

    [VOID-35] Reworked spawner data handling

--- Assets/Scripts/Level/EnemySpawner.cs
+++ Assets/Scripts/Level/EnemySpawner.cs
@@ -20,28 +20,15 @@ namespace Level
             public float weight;
         }
 
-        public SpawnData walker;
-        public SpawnData crawler;
-        public SpawnData juggernaut;
-        
         //GameObject: Type of Enemy, float: Weight
-        private Dictionary<GameObject, float> _enemyDictionary = new Dictionary<GameObject, float>();
-        private WeightedArray<GameObject> _enemyWeightedList = new WeightedArray<GameObject>();
+        private List<SpawnData> _enemies = new();
+        private WeightedArray<SpawnData> _enemyWeightedList = new();
 
         private void Awake() {
-            _enemyDictionary.Add(walker.prefab, walker.weight);
-            _enemyDictionary.Add(crawler.prefab, crawler.weight);
-            _enemyDictionary.Add(juggernaut.prefab, juggernaut.weight);
-
-            foreach (var enemy in _enemyDictionary)
-            {
-                _enemyWeightedList.AddElement(enemy.Key, enemy.Value);
+            foreach (var enemy in _enemies) {
+                _enemyWeightedList.AddElement(enemy, enemy.weight);
             }
-        }
-
-        private void Start() {
-            var enemyToSpawn = _enemyWeightedList.GetRandomItem();
-            Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+            Instantiate(_enemyWeightedList.GetRandomItem().prefab, transform.position, Quaternion.identity);
         }
     }
 }
