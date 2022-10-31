17e54eea04be42b9fdcc73ca64338fc41f88c988
Griffin <51525281+duong-lc@users.noreply.github.com>
2022-10-31 15:22:21 +0700

    Base Pooler added

---
+++ Assets/Scripts/ParticleTest/ParticleManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Patterns;
+
+namespace Level
+{
+    public class ParticleManager : PoolBase<ParticleTest>
+    {
+        [SerializeField] private ParticleTest particlePrefab;
+    }
+}
