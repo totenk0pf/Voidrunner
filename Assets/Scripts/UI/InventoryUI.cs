edd6cdcec5b0798a0ec4743ec323780cf404a555
totenk0pf <nguyenhuuquang2608@gmail.com>
2022-11-03 17:57:49 +0700

    [VOID-22] bukutanmoyden

---
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    public class InventoryUI : MonoBehaviour {
+        [SerializeField] private Slider weightSlider;
+        [SerializeField] private GameObject itemPrefab;
+
+    }
+}
