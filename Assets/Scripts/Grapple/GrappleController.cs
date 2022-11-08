86a348e9393b706718fa08c13b56052b6a94a0ed
Griffin <51525281+duong-lc@users.noreply.github.com>
2022-11-08 23:00:21 +0700

    Finish AA for grapple

--- Assets/Scripts/Grapple/GrappleController.cs
+++ Assets/Scripts/Grapple/GrappleController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Core.Logging;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 
 namespace Grapple
@@ -15,27 +16,24 @@ namespace Grapple
         [Header("Grapple Attributes")] 
         [SerializeField] private float range;
         [SerializeField] private float radius;
-        [SerializeField] private List<LayerMask> masks;
+        [SerializeField] private float maxAngleAA = 5f;
 
-        private LayerMask _grappleLayer;
+        [SerializeField] private LayerMask grappleLayer;
+        [SerializeField] private LayerMask ignoreLayer;
         private Vector3 _castOrigin;
         private Camera _mainCam;
         private Vector3 _collisionPos;
-        private RaycastHit[] _results = new RaycastHit[10];
-        
+
+        public GameObject currentGrappleCollider;
         
         private void Start()
         {
             _mainCam = Camera.main;
             if (!_mainCam) NCLogger.Log("Can't find Camera.Main", LogLevel.ERROR);
-
-            foreach (var mask in masks) {
-                _grappleLayer += mask;
-            }
+            
         }
-         
-        
-        
+
+
         // Update is called once per frame
         private void Update()
         {
@@ -45,29 +43,66 @@ namespace Grapple
             }
         }
 
-        private void CastToGetGrappleLocation()
+        private bool CastToGetGrappleLocation()
         {
             var lookDir = _mainCam.transform.forward;
             _castOrigin = UpdateCastOrigin();
-            
-            var iteration = Physics.CapsuleCastNonAlloc(_castOrigin, _castOrigin, radius, lookDir, _results, range);
 
-            if (iteration <= 0) {
-                NCLogger.Log($"iteration of hits = {iteration}", LogLevel.INFO); 
-                return;
+            var results = Physics.CapsuleCastAll(_castOrigin, _castOrigin, radius, lookDir, range, ~ignoreLayer);
+            if (results.Length <= 0) {
+                _collisionPos = Vector3.zero;
+                return false;
             }
             
-            RaycastHit hit = _results[0];
-            for (var i = 0; i < iteration; i++) {
-                if (_results[i].distance < hit.distance) hit = _results[i];
-            }
-            _collisionPos = hit.point;
+            //Sort by distance, from closest to furthest
+            Array.Sort(results, (hit1, hit2) => hit1.distance.CompareTo(hit2.distance));
+
+            var hit = new RaycastHit();
+            var checkIndex = 0;
             
+            for (var i = 0; i < results.Length; i++) {
+                if (!IsInGrappleMask(results[i].collider.gameObject.layer)) 
+                    continue;
+
+                hit = results[i];
+                checkIndex = i;
+                break;
+            }
+            if (!hit.collider) return false;
+            var tuple = GetHitPointAimAssisted(hit, checkIndex, lookDir);
+
+            _collisionPos = tuple.Item1 ? tuple.Item2 : Vector3.zero;
+            
+            return tuple.Item1;
         }
 
 
-        private Vector3 UpdateCastOrigin()
+        private Tuple<bool, Vector3> GetHitPointAimAssisted(RaycastHit hit1, int checkIndex, Vector3 lookDir)
         {
+            var tupleSuccess = new Tuple<bool, Vector3>(true, hit1.point);
+            var tupleFail = new Tuple<bool, Vector3>(false, Vector3.zero);
+            var hit2 = new RaycastHit();
+            
+            //if grapple point is closest -> success
+            if (checkIndex == 0) return tupleSuccess;
+
+            //secondary raycast to make sure the grapple point is in field of vision
+            if (!Physics.Linecast(_castOrigin, hit1.collider.transform.position, out hit2, ~ignoreLayer)) return tupleFail;
+            if(hit2.collider) if(!IsInGrappleMask(hit2.collider.gameObject.layer)) return tupleFail;
+
+            //angle between cam.fwd and direction(cam to hit1.point)
+            var angle = Vector3.Angle(hit1.point - _castOrigin, _mainCam.transform.forward);
+            //return based on angle
+            if (!(angle <= maxAngleAA)) return tupleFail;
+            currentGrappleCollider = hit2.collider.gameObject; 
+            return hit2.collider == hit1.collider ? tupleSuccess : tupleFail;
+        }
+
+        private bool IsInGrappleMask(int layer) {
+            return grappleLayer == (grappleLayer | (1 << layer));
+        }
+        
+        private Vector3 UpdateCastOrigin() {
             return _mainCam.transform.position;
         }
         
@@ -77,8 +112,8 @@ namespace Grapple
             Gizmos.DrawWireSphere(Camera.main.transform.position, radius);
             Gizmos.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * range);
             Gizmos.DrawWireSphere(_collisionPos, radius);
-            //Gizmos.DrawRay(_castOrigin, );
         }
     }
 
 }
+    
