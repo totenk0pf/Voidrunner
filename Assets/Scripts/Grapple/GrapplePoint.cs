e4746c2a4996da778d18d8d642b2ecaf5e0eb24f
Griffin <51525281+duong-lc@users.noreply.github.com>
2022-11-10 16:05:22 +0700

    Initializing Grapple Point ground auto snap

--- Assets/Scripts/Grapple/GrapplePoint.cs
+++ Assets/Scripts/Grapple/GrapplePoint.cs
@@ -1,9 +1,36 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Grapple
 {
     public class GrapplePoint : MonoBehaviour
     {
+        [SerializeField] private Vector3 dimension;
+        [SerializeField] private Vector3 castOrigin = Vector3.zero;
+        [SerializeField] private float castDistance;
+        [SerializeField] private LayerMask ignoreMask;
         
+        private void Awake()
+        {
+            if (castOrigin == Vector3.zero) castOrigin = transform.position;
+        }
+
+        public Tuple<bool, Vector3> GetAvailableSurface()
+        {
+            var returnTuple = new Tuple<bool, Vector3>(false, Vector3.zero);
+            Physics.BoxCast(transform.position, dimension, Vector3.down, Quaternion.identity, castDistance, ~ignoreMask);
+            
+            
+            
+            return returnTuple;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube();
+        }
     }
 }
