f9eea2453da903ccfeca255e0b2ee7cbeb79a801
legendofnothing <huyrtb123@gmail.com>
2022-11-24 15:06:39 +0700

    Add Prototype model for Juggernaut and anims (still bug w/ stuck in idle)

--- Assets/Scripts/Entities/Enemy/Juggernaut/JuggernautAttack.cs
+++ Assets/Scripts/Entities/Enemy/Juggernaut/JuggernautAttack.cs
@@ -1,36 +1,71 @@
 using System.Collections;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class JuggernautAttack : EnemyState
-{
-    public float attackDelay;
+namespace Entities.Enemy.Juggernaut {
+    public class JuggernautAttack : EnemyState
+    {
+        public float attackDelay;
     
-    [SerializeField] private EnemyState _previousState;
-    [SerializeField] private EnemyState _nextState;
+        [TitleGroup("Attack settings")]
+        [SerializeField] private EnemyState _previousState;
+        [SerializeField] private EnemyState _nextState;
 
-    private bool _isAttacking = false;
+        [TitleGroup("Attack sets")] [SerializeField]
+        private AnimSerializedData animData; 
 
-    public override EnemyState RunCurrentState() {
-        //Change 2f number if changed in WalkerHostile also 
-        if (Vector3.Distance(transform.position, target.transform.position) > 3f) {
-            return _previousState;
+        private bool _isAttacking;
+        private bool _canChangeState;
+        private bool _canAttack = true;
+
+        public override EnemyState RunCurrentState() {
+            if (_canChangeState) {
+                _canChangeState = false;    
+                return _previousState;
+            }
+
+            if (_canAttack) {
+                StartCoroutine(StartAttack());
+            }
+
+            return this;
         }
 
-        if (!_isAttacking) {
-            StartCoroutine(DamagePlayer());
+        IEnumerator StartAttack() {
+            _canAttack = false;
+            _isAttacking = true;
+            
+            var randomAttack = Random.Range(0, animData.attackAnim.Count);
+            var attack = animData.attackAnim[randomAttack];
+            
+            Agent.enabled = false;
+            
+            TriggerAnim(attack);
+            yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.95f);
+            
+            _canAttack = true;
+            _isAttacking = false;
+
+            var dist = Vector3.Distance(transform.position, target.transform.position);
+            if (dist > 2f && !_isAttacking)
+            {
+                Agent.enabled = true;
+                Agent.ResetPath();
+                _canChangeState = true;
+                _canAttack = false;
+            }
+
+            else yield return DelayAttack();
         }
 
-        return this;
-    }
+        private IEnumerator DelayAttack()
+        {
+            yield return new WaitForSeconds(0.2f);
+            yield return StartCoroutine(StartAttack());
+        }
 
-    IEnumerator DamagePlayer() {
-        _isAttacking = true;
-
-        var oxygenComp = target.GetComponent<Oxygen>();
-        oxygenComp.ReducePermanentOxygen(enemyBase.enemyDamage);
-
-        yield return new WaitForSeconds(attackDelay);
-
-        _isAttacking = false;
+        //TODO: Spin Attack for juggernaut
+        //TODO: Slam Attack for juggernaut
+    
     }
 }
--- Assets/Scripts/Entities/Enemy/Juggernaut/JuggernautHostile.cs
+++ Assets/Scripts/Entities/Enemy/Juggernaut/JuggernautHostile.cs
@@ -1,19 +1,24 @@
+using Entities.Enemy;
 using UnityEngine;
 
 public class JuggernautHostile : EnemyState
 {
     public float fastChaseSpeed;
     [SerializeField] private EnemyState _nextState;
+    [SerializeField] private AnimSerializedData animData;
     
     public override EnemyState RunCurrentState() {
-        Agent.SetDestination(target.transform.position);
-        Agent.isStopped = false;
-        Agent.speed = fastChaseSpeed;
-
-        if (Agent.remainingDistance < 3f) {
+        if (!animator.GetBool(animData.hostileAnim.name)) TriggerAnim(animData.hostileAnim);
+        if (Agent.enabled) Agent.SetDestination(target.transform.position);
+  
+        if (GetPathRemainingDistance(Agent) < 3f && GetPathRemainingDistance(Agent) > -1)
+        {
+            TriggerAnim(animData.hostileAnim);
             Agent.isStopped = true;
             return _nextState;
         }
+            
+        Agent.isStopped = false;
         return this;
     }
 }
