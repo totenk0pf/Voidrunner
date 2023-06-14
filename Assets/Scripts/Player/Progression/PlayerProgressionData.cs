050cc757a1dcdfbcb64551ffa3954f990d017a03
totenk0pf <nguyenhuuquang2608@gmail.com>
2023-06-14 09:17:32 +0700

    Fixing more merge bullshit

---
+++ Assets/Scripts/Player/Progression/PlayerProgressionData.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[Serializable]
+public class EnemyExperienceData {
+    [ValueDropdown("GetEnemyTags", DropdownTitle = "Select enemy type")]
+    public string tag;
+    public float gain;
+    private IEnumerable<string> GetEnemyTags() {
+        return UnityEditorInternal.InternalEditorUtility.tags.Where(x => x.ToLower().Contains("enemy"));
+    }
+}
+
+[CreateAssetMenu(fileName = "PlayerProgressionData", menuName = "Progression/Data", order = 0)]
+public class PlayerProgressionData : ScriptableObject {
+    [TitleGroup("Level")] 
+    public int defaultLevel; 
+    public int maxLevel;
+
+    [TitleGroup("Experience")] 
+    public float baseXPGain;
+    public float XPGainMod;
+
+    [TitleGroup("Enemy settings")] 
+    public List<EnemyExperienceData> enemyExperienceData;
+}
